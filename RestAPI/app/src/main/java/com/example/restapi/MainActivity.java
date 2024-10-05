05880b179ea5408a8cbdb20a4500d8274b42945a
AritraSanyal <aritras2003@gmail.com>
2024-10-05 10:00:44 +0530

    added permissions, code for fetching api and dependency for 3rd party api(volley).

--- RestAPI/app/src/main/java/com/example/restapi/MainActivity.java
+++ RestAPI/app/src/main/java/com/example/restapi/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.restapi;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +9,46 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+
 public class MainActivity extends AppCompatActivity {
 
+    private RequestQueue requestQueue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        requestQueue = Volley.newRequestQueue(MainActivity.this);
+        getPokemonList();
+    }
+    public void getPokemonList(){
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest("https://dummyapi.online/api/pokemon", new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                try {
+
+                    Log.d("TAG","onResponse: try block"+ response);
+
+                } catch (Exception e){
+                    Log.d("TAG","onResponse: catch block"+ e);
+                }
+
+            }}, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("TAG","onErrorResponse: "+ error);
+
+            }
+
         });
+        requestQueue.add(jsonArrayRequest);
     }
 }
