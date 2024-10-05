d334d85e1b87af72f56b02b8c6850ed45c705038
AritraSanyal <aritras2003@gmail.com>
2024-10-05 09:10:54 +0530

    created new project and updated previous

--- RecycleView/app/src/main/java/Adapter/MyAdapter.java
+++ RecycleView/app/src/main/java/Adapter/MyAdapter.java
@@ -1,26 +1,49 @@
 package Adapter;
 
-import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-public class MyAdapter extends RecyclerView.Adapter{
+
+import com.example.recycleview.R;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>{
 
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+       View cardView = LayoutInflater.from(parent.getContext()).inflate(R.layout.card, parent, false);
+        return new MyViewHolder(cardView);
+
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
 
+        holder.txtTitle.setText("I have been changed from adapter");
+        holder.txtDescription.setText("I have also been changed from adapter");
     }
+
 
     @Override
     public int getItemCount() {
-        return 0;
+        return 30;
     }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        private TextView txtTitle, txtDescription;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            txtTitle = itemView.findViewById(R.id.txtTitle);
+            txtDescription= itemView.findViewById(R.id.txtDescription);
+        }
+    }
+
 }
--- RecycleView/app/src/main/java/com/example/recycleview/MainActivity.java
+++ RecycleView/app/src/main/java/com/example/recycleview/MainActivity.java
@@ -7,18 +7,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import Adapter.MyAdapter;
+import Models.ListItems;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView rcView;
+    private MyAdapter adapter;
+
+    private List<ListItems> arrayList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        rcView = findViewById(R.id.rcView);
+        rcView.setHasFixedSize(true);
+        rcView.setLayoutManager(new LinearLayoutManager(this));
+
+        for(int i = 0; i <= 10; i++){
+            ListItems items = new ListItems("Title " + i+1, "description"+)
+        }
+
+        adapter = new MyAdapter(arrayList);
+        rcView.setAdapter(adapter);
+
+
     }
 }
