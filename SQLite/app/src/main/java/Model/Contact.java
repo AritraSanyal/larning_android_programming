62417afb6f68ec8521f9dd00210cf5120d5d5e4b
AritraSanyal <aritras2003@gmail.com>
2024-10-12 22:56:46 +0530

    added model class contact

---
+++ SQLite/app/src/main/java/Model/Contact.java
@@ -0,0 +1,48 @@
+package Model;
+
+public class Contact {
+
+    private String name, phoneNo;
+    private int id;
+
+
+    public Contact() {
+
+
+    }
+
+    public Contact(String name, String phoneNo) {
+        this.name = name;
+        this.phoneNo = phoneNo;
+    }
+
+    public Contact(String name, String phoneNo, int id) {
+        this.name = name;
+        this.phoneNo = phoneNo;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
