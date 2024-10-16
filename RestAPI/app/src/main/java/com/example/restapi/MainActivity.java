f21fa195059af357c3fb99597e274321badc543f
AritraSanyal <aritras2003@gmail.com>
2024-10-16 23:48:05 +0530

    added swipe to delete

--- RestAPI/app/src/main/java/com/example/restapi/MainActivity.java
+++ RestAPI/app/src/main/java/com/example/restapi/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Looper;
 import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -25,6 +26,16 @@ import java.util.List;
 
 import Adapter.PokemonAdapter;
 import Model.Pokemon;
+
+
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.RecyclerView;
+import android.graphics.Canvas;
+import androidx.core.content.ContextCompat;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -56,10 +67,69 @@ public class MainActivity extends AppCompatActivity {
         // Initialize adapter and set it to RecyclerView
         pokemonAdapter = new PokemonAdapter(this, pokemonList);
         pokemonRecyclerView.setAdapter(pokemonAdapter); // Set the adapter
+        // Attach the ItemTouchHelper to the RecyclerView
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleItemTouchCallback);
+        itemTouchHelper.attachToRecyclerView(pokemonRecyclerView);
 
         // Fetch the initial Pokemon list
         getPokemonList();
+
+        // Enable Swipe-to-Delete functionality
+        enableSwipeToDelete();
     }
+
+    ItemTouchHelper.SimpleCallback simpleItemTouchCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+            // We are not handling drag & drop, so return false
+            return false;
+        }
+
+        @Override
+        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+            // Remove the swiped item from the adapter
+            int position = viewHolder.getAdapterPosition();
+            pokemonList.remove(position);
+            pokemonAdapter.notifyItemRemoved(position);
+        }
+
+        @Override
+        public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView,
+                                @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY,
+                                int actionState, boolean isCurrentlyActive) {
+            // Optional: Custom draw code for swipe background or icon
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+        }
+    };
+
+    private void enableSwipeToDelete() {
+        ItemTouchHelper.SimpleCallback simpleItemTouchCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false; // We don't need drag & drop functionality
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                // Get the swiped item's position
+                int position = viewHolder.getAdapterPosition();
+
+                // Remove the item from the dataset
+                pokemonList.remove(position);
+
+                // Notify the adapter about the removal
+                pokemonAdapter.notifyItemRemoved(position);
+            }
+            @Override
+            public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView,
+                                    @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY,
+                                    int actionState, boolean isCurrentlyActive) {
+                // Custom drawing code for swipe (optional)
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+            }
+        };
+    }
+
 
     @Override
     protected void onStart() {
